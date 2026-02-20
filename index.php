065ac9c5833d8e1c0c1b4d55aaa1ff61ef066add
luimstumbad00 <aguilar.torres.luis.06@gmail.com>
2026-02-19 21:29:35 -0600

    Nueva actualizacion

--- index.php
+++ index.php
@@ -3,5 +3,7 @@ echo "<h1> Hola vros, escriban sus nombres para confirmar<h1>";
 echo "<h1> Lois<h1>";
 echo "<h1> Roman<h1>";
 echo "<h1> Charles<h1>";
+echo "<h1> Ulises nos va a reprobar<h1>";
+
 
 ?>
